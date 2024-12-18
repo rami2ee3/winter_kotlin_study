9ac21e61996664afec3749de8ea1b32f0599cf95
rami <122771812+rami2ee3@users.noreply.github.com>
2024-12-19 02:24:33 +0900

    test: 테스트 코드 추가

--- src/test/kotlin/day03/ClericTest.kt
+++ src/test/kotlin/day03/ClericTest.kt
@@ -17,6 +17,26 @@ class ClericTest {
     }
 
     @Test
+    fun selfAid2() {
+        //given 준비
+        val cleric = Cleric("용사", hp = 50, mp = 5)
+        //when 실행
+        cleric.selfAid()
+        //then 검증
+        assertEquals(50, cleric.hp)
+    }
+
+    @Test
+    fun selfAid3() {
+        //given 준비
+        val cleric = Cleric("용사", hp = 30, mp = 3)
+        //when 실행
+        cleric.selfAid()
+        //then 검증
+        assertEquals(50, cleric.hp)
+    }
+
+    @Test
     fun pray() {
         //given 준비
         val cleric = Cleric("용사", mp = 6)
