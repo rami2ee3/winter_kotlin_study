f67ec3a93cab7890392c6cb7c78c5598bdaef2a2
rami <122771812+rami2ee3@users.noreply.github.com>
2025-01-04 00:28:53 +0900

    refactor: CoroutineBirds 코드 변경

--- src/main/kotlin/day13/CoroutineBirds.kt
+++ src/main/kotlin/day13/CoroutineBirds.kt
@@ -4,33 +4,67 @@ package day13
 // 각 코루틴은 4번만 출력한 후 완료되어야 합니다. -> 각 새마다 4번씩 울음
 // 첫 번째 새는 "꾸우" 소리를 냅니다. 두 번째 새는 "까악" 소리를 냅니다. 마지막 새는 "짹짹" 소리를 냅니다.
 // --------------------------------------------------
-// 이전 과제를 확장하여 콘솔에 4번만 출력하는 제한을 제거
+// 이전 과제를 확장하여 콘솔에 4번만 출력하는 제한을 제거 -> 각 코루틴은 무기한으로 출력
 // 10초 후에 모든 실행 중인 코루틴을 취소하는 메커니즘을 추가
 
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 
 suspend fun kkou() {
-    repeat(4) {
+    while (true) {
         println("꾸우")
         delay(1000)
     }
 }
 
 suspend fun kkack() {
-    repeat(4) {
+    while (true) {
         println("까악")
         delay(2000)
     }
 }
 
 suspend fun jjack() {
-    repeat(4) {
+    while (true) {
         println("짹짹")
         delay(3000)
     }
 }
+
+fun main() = runBlocking {
+    val job = launch {
+        launch { kkou() }
+        launch { kkack() }
+        launch { jjack() }
+    }
+    delay(10_000)
+    job.cancelAndJoin() // 각각 따로 호출하지 않고 단일 메서드로 처리 가능. 코루틴을 취소하고, 모든 작업이 종료될 때까지 기다린다.
+    println("10초가 경과하여 작업을 종료합니다.")
+}
+
+
+//fun main() = runBlocking {
+//    val job = launch {
+//        val time = measureTimeMillis {
+//            val result1 = kkou()
+//            val result2 = kkack()
+//            val result3 = jjack()
+//        }
+//        if (time >= 10000) {
+//            cancel()
+//        }
+//    }
+//}
+//---------------------------------------------
+//fun main() {
+//    runBlocking {
+//        val job: Job = launch {
+//            launch { kkou() }
+//            launch { kkack() }
+//            launch { jjack() }
+//        }
+//        job.cancel()
+//    }
+//}
 
 //fun main():Unit = runBlocking {
 //    launch { kkou() }
@@ -38,10 +72,3 @@ suspend fun jjack() {
 //    launch { jjack() }
 //}
 
-fun main() {
-    runBlocking {
-        launch { kkou() }
-        launch { kkack() }
-        launch { jjack() }
-    }
-}
---
+++ src/test/kotlin/day13/MovieKtTest.kt
@@ -0,0 +1,22 @@
+package day13
+
+import kotlinx.coroutines.runBlocking
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class MovieKtTest {
+
+    @Test
+    fun checkGetMovieInfo() = runBlocking {
+        val movie = getMovieInfo()
+
+        val expectedTitle = "Star wars"
+        val expectedDirector = "George Lucas"
+        val expectedYear = 1977
+
+        assertEquals(expectedTitle, movie.title)
+        assertEquals(expectedDirector, movie.director)
+        assertEquals(expectedYear, movie.year)
+    }
+}
